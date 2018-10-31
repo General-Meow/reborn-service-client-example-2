7073e0b84b20066e5151393039b1b373b456c23e
General-Meow <spl.ivalis@gmail.com>
2018-10-31 22:05:46 +0000

    working example of refreshing config with configuration service

--- src/main/java/com/example/rebornserviceclientexample2/RebornServiceClientExample2Application.java
+++ src/main/java/com/example/rebornserviceclientexample2/RebornServiceClientExample2Application.java
@@ -3,6 +3,7 @@ package com.example.rebornserviceclientexample2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient
 @RestController
 @SpringBootApplication
+@RefreshScope
 public class RebornServiceClientExample2Application {
 
 	public static void main(String[] args) {
